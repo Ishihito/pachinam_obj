e8f81ea23e5f3087adbbd95f04a4a97911d8acb6
Ishihito <peteripenev@gmail.com>
2018-03-16 18:33:33 -0400

    Update pachinam_obj.py

--- pachinam_obj.py
+++ pachinam_obj.py
@@ -7,16 +7,20 @@ from Bio.PDB import *
 from Bio.PDB.MMCIF2Dict import MMCIF2Dict
 
 """
-To be used from within PyMOL script, right after cif files have been loaded into objects 
-and alignment has been done
-Uses the already defined objects to further define their chains into separate objects
-with names pulled from the cif files
-Hard-codes the destination of cif files.
+# pachinam_obj.py
+To be used from within PyMOL, right after cif files have been loaded into objects
+Uses the already defined objects to pull pairs of chain identifiers and names from cif files and create separate objects - essentially does the job of split_chains but adds the defined name from the cif file.
+Names are of the format:
+cifID_name_chainID
+Hard-codes destination of cif files to a directory which is called cif and is within the current working directory - this means you need to have the cif files in this directory which is in the working directory (since the script is being ran from within PyMOL this would be the current working directory of PyMOL)
+Tries to remove some lengthy parts of names often found in big structure files (e.g. 30S ribosomal protein, ribosomal RNA etc.) for better viewing in PyMOL.
+Has been tested to work up to version 2.1.0 in PyMOL
+Uses biopython and python3 so have them installed.
 """
 
 parser = MMCIFParser(QUIET=True)
 
-string = "../cif/"
+string = "./cif/"
 fileext = ".cif"
 cifs = list(cmd.get_object_list())
 newcifs = [x + fileext for x in cifs]
